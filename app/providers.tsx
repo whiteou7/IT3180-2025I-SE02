b6dd2920bbeab60f19339a057a31c932cda814e2
whiteou7 <fps4day@gmail.com>
2025-10-17 18:30:57 +0700

    init project

---
+++ app/providers.tsx
@@ -0,0 +1,12 @@
+"use client"
+// app/providers.tsx
+
+import {HeroUIProvider} from '@heroui/react'
+
+export function Providers({children}: { children: React.ReactNode }) {
+  return (
+    <HeroUIProvider>
+      {children}
+    </HeroUIProvider>
+  )
+}
