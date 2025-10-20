0bf2205fb8dd834b8110bd52bcacc0e6706537ff
haianh020505 <lehaianh252005@gmail.com>
2025-10-20 10:07:18 +0700

    feat: Add API to get all users

---
+++ pages/api/users/listAllUsers.ts
@@ -0,0 +1,37 @@
+import type { NextApiRequest, NextApiResponse } from "next"
+import { db } from "@/db" 
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") {
+    res.setHeader("Allow", ["GET"]);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+
+  try {
+    const users = await db`
+      SELECT 
+        user_id, 
+        email, 
+        full_name, 
+        role,
+        year_of_birth
+      FROM users;
+    `;
+
+    return res.status(200).json({
+      success: true,
+      message: "User list fetched successfully.",
+      data: users
+      })
+
+  } catch (error) {
+    console.error("Error fetching user list:", error);
+    return res.status(500).json({
+      success: false,
+      message: (error as Error).message,
+    })
+  }
+}
