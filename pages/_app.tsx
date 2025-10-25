fffcbc9e792b9d2fac26e571f6a849bafe83590d
whiteou7 <fps4day@gmail.com>
2025-10-26 01:33:52 +0700

    feat: add some router rules

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,30 @@
 // pages/_app.tsx
 import type { AppProps } from "next/app"
+import { useEffect } from "react"
+import { useRouter } from "next/router"
 import "../styles/globals.css"
 import { Toaster } from "sonner"
 import Header from "../components/Header"
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const userId = localStorage.getItem("userId")
+
+    if (userId && router.pathname === "/") {
+      router.push("/feed")
+    }
+  }, [router])
+
   return (
     <>
       <Header />
       <main className="pt-16">
-        < Component {...pageProps} />
+        <Component {...pageProps} />
       </main>
-      <Toaster position="top-center" richColors /></>
+      <Toaster position="top-center" richColors />
+    </>
   )
 }
 
