b714fc3d07bf0f8f433410a06b14b5e9a952fc10
whiteou7 <fps4day@gmail.com>
2025-10-26 12:24:55 +0700

    forgot to commit api

--- pages/api/users/[id].ts
+++ pages/api/users/[id].ts
@@ -6,13 +6,20 @@ import { APIBody } from "@/types/api"
 
 /**
  * PUT /api/users/[id] - Update user information
+ * GET /api/users/[id] - Get user info 
  */
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<APIBody<User>>
 ) {
+  const { id: userId } = req.query
+  if (!userId) {
+    return res.status(400).json({
+      success: false, 
+      message: "User ID is required" 
+    })
+  }
   try {
-    const { id: userId } = req.query
     if (req.method === "PUT") {
       const { email, fullName, role, yearOfBirth, gender } = req.body as {
         email: string;
@@ -22,28 +29,23 @@ export default async function handler(
         gender: "male" | "female";
       }
 
-      if (!userId) {
-        return res.status(400).json({
-          success: false, 
-          message: "User ID is required" 
-        })
-      }
       const updatedUser = await db<User[]>`
-          UPDATE users
-          SET
-            email = ${email},
-            full_name = ${fullName},
-            role = ${role},
-            year_of_birth = ${yearOfBirth},
-            gender = ${gender}
-          WHERE user_id = ${userId}
-          RETURNING 
-            email,
-            full_name,
-            role,
-            year_of_birth,
-            gender;
-        `
+        UPDATE users
+        SET
+          email = ${email},
+          full_name = ${fullName},
+          role = ${role},
+          year_of_birth = ${yearOfBirth},
+          gender = ${gender}
+        WHERE user_id = ${userId}
+        RETURNING 
+          user_id,
+          email,
+          full_name,
+          role,
+          year_of_birth,
+          gender;
+      `
 
       if (updatedUser.length === 0) {
         return res.status(404).json({ 
@@ -57,6 +59,32 @@ export default async function handler(
         message: "Updated user info",
         data: updatedUser[0]
       })
+    } else if (req.method == "GET") {
+      const user = await db<User[]>`
+        SELECT 
+          user_id,
+          email,
+          full_name,
+          role,
+          year_of_birth,
+          gender
+        FROM users
+        WHERE user_id = ${userId};
+      `
+
+      if (user.length === 0) {
+        return res.status(404).json({
+          success: false,
+          message: "User not found",
+        })
+      }
+
+      return res.status(200).json({
+        success: true,
+        message: "Fetched user info",
+        data: user[0],
+      })
+
     } else {
       res.setHeader("Allow", ["PUT"])
       return res.status(405).json({
