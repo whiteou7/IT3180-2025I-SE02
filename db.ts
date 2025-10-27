3271da7f1f3fe5a53db765929dfb0a37bd2791ac
whiteou7 <fps4day@gmail.com>
2025-10-27 18:21:41 +0700

    feat: update supabase credentials

--- db.ts
+++ db.ts
@@ -15,3 +15,13 @@ const db = postgres(process.env.DATABASE_URL, {
 })
 
 export { db }
+
+import { createClient } from "@supabase/supabase-js"
+
+if (!process.env.SUPABASE_URL || !process.env.SUPABASE_KEY) {
+  throw new Error("DATABASE_URL environment variable is not defined")
+}
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseAnonKey = process.env.SUPABASE_KEY
+
+export const storage = createClient(supabaseUrl, supabaseAnonKey).storage
