2a77d48c579dd628ce4629534edc6238573dd4c1
whiteou7 <fps4day@gmail.com>
2025-11-27 03:36:09 +0700

    fix: dont block api requests during preview mode

--- middleware.js
+++ middleware.js
@@ -3,10 +3,5 @@ import { NextResponse } from "next/server"
 export function middleware(request) {
   const { method, nextUrl } = request
 
-  // Only apply to API routes
-  if (nextUrl.pathname.startsWith("/api") && method !== "GET" && process.env.PREVIEW_MODE == "true") {
-    return NextResponse.json({ message: "Blocking all requests during preview" }, { status: 405 })
-  }
-
   return NextResponse.next()
 }
