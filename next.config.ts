b7fe7bca066d6de2a861c1ee2e06634ac0e38a9e
whiteou7 <fps4day@gmail.com>
2025-12-27 17:29:44 +0700

    ??

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  experimental: {
-    serverComponentsExternalPackages: ["@h1dd3nsn1p3r/pdf-invoice", "pdfmake"],
-  },
 }
 
 export default nextConfig
